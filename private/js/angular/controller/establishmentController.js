04aba3e7a032a4fb69a9ccdf477bc5fe38493b34
Maxi - PC <Maxi - PC>
2017-02-16 00:00:34 +0100

    homedone

---
+++ private/js/angular/controller/establishmentController.js
@@ -0,0 +1,12 @@
+myApp.controller('establishmentController', function ($scope) {
+
+    $scope.establishments = [
+
+        {name: 'News', color:'blue'},
+        {name: 'Vegetarisch', color:'orange'},
+        {name: 'Test', color:'blue'},
+        {name: 'Testöö', color:'orange'}
+
+
+    ]
+});
