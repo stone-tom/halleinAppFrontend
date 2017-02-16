15c2b73efe2e3eb644059122c68466fcb6e6390a
Maxi - PC <Maxi - PC>
2017-02-16 01:23:09 +0100

    establishment

--- private/js/angular/controller/establishmentController.js
+++ private/js/angular/controller/establishmentController.js
@@ -1,12 +1,13 @@
 myApp.controller('establishmentController', function ($scope) {
 
-    $scope.establishments = [
+    $scope.sexes = {
+options:
+[
+        {name: 'MÄNNL.'},
+        {name: 'WEIBL.'}]
 
-        {name: 'MÄNNL.', color:'blue'},
-        {name: 'Vegetarisch', color:'orange'},
-        {name: 'Test', color:'blue'},
-        {name: 'Testöö', color:'orange'}
+}
 
 
-    ]
+
 });
