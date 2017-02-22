9037dde479a34577f024f9ec4cbd1b19c0dd8b19
Matthias Lang <langmattis@gmail.com>
2017-02-22 15:44:49 +0100

    Meals in restaurants

--- private/js/angular/controller/foodRestaurantsController.js
+++ private/js/angular/controller/foodRestaurantsController.js
@@ -1,6 +1,27 @@
 myApp.controller('foodRestaurantsController', function ($scope, $http) {
     $http.get(URL + '/restaurants?get=id,restaurantname')
         .then(function (response) {
-            $scope.restaurants = response.data;
+            var restaurants = response.data;
+            var today = new Date();
+            var data = {
+                date: dateToString(today)
+            }
+            $http({
+                url: URL + '/menus',
+                method: 'GET',
+                params: data
+            }).then(function (response) {
+                    var menus = response.data;
+                    for(var i = 0; i < menus.length; i++){
+                        for(var j = 0; j < restaurants.length; j++){
+                            if(menus[i].restaurant == restaurants[j].id){
+                                restaurants[j].menu = menus[i];
+                            }
+                        }
+                        if(i == menus.length - 1){
+                            $scope.restaurants = restaurants;
+                        }
+                    }
+                })
         })
 });
