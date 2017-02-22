c611b7fb9d1e337861c805484ff3d754f101e309
Matthias Lang <langmattis@gmail.com>
2017-02-22 11:47:16 +0100

    Changed a to div on restaurants

--- private/js/angular/controller/foodFeedbackController.js
+++ private/js/angular/controller/foodFeedbackController.js
@@ -1,5 +1,5 @@
 myApp.controller('foodFeedbackController', function ($scope, $http) {
-    $http.get(URL + '/feedback?get=restaurant,rating,subject,text&status=2')
+    $http.get(URL + '/feedback?get=restaurant,rating,text&status=2')
         .then(function (response) {
             $scope.bubbles = response.data;
         })
