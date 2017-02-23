86faf2ce1dab52dbf3065bef1fd00f38bcb5cb1e
Thomas Steiner <stone.tom.email@gmail.com>
2017-02-23 16:11:19 +0100

    feedback filtering done

--- private/js/angular/controller/foodFeedbackController.js
+++ private/js/angular/controller/foodFeedbackController.js
@@ -1,6 +1,34 @@
 myApp.controller('foodFeedbackController', function ($scope, $http) {
-    $http.get(URL + '/feedback?get=restaurant,rating,text&status=2')
+
+    getFeedback('-created');
+
+    $scope.feedbackSlider = {
+        current: 0,
+        option: [
+            {name: 'Neueste', orderBy:'-created'},
+            {name: 'Beste', orderBy:'-rating'}
+        ]
+    }
+    $scope.feedbacksliderUp = function () {
+        if ($scope.feedbackSlider.current == 0) {
+            $scope.feedbackSlider.current = $scope.feedbackSlider.option.length -1;
+
+        }
+        else {
+            $scope.feedbackSlider.current--
+
+        }
+        getFeedback($scope.feedbackSlider.option[$scope.feedbackSlider.current].orderBy);
+
+    }
+
+    function getFeedback(orderBy) {
+        $http.get(URL + '/feedback?get=restaurant,rating,text&status=2&orderBy=' + orderBy)
         .then(function (response) {
             $scope.bubbles = response.data;
         })
+
+    }
+
+
 });
