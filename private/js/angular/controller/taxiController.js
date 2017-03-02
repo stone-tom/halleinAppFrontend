6b98777b955e76889498adfb8e8b8f13f95dde03
Matthias Lang <langmattis@gmail.com>
2017-03-02 15:32:37 +0100

    Taxi click animation

--- private/js/angular/controller/taxiController.js
+++ private/js/angular/controller/taxiController.js
@@ -1,9 +1,9 @@
 myApp.controller('taxiController', function ($scope) {
     $scope.taxis = [
         {name: 'Taxi Resul', phone: '71111'},
-        {name: 'City Taxi Hallein', phone: '82030'},
+        {name: 'City Taxi', phone: '82030'},
         {name: "DES IS'A TAXI", phone: '70400'},
-        {name: "Taxi Hallein Steiner", phone: '70400'},
+        {name: "Taxi Steiner", phone: '70400'},
         {name: "Taxi Comfortline", phone: '70400'},
         {name: "Abdinghoff Taxi", phone: '70400'},
         {name: "Alfred Aschauer", phone: '70400'}
